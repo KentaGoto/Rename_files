72fd7967e115094eceb38268a8eb08119bafe283
KentaGoto <gotokentag@gmail.com>
2021-02-03 09:45:31 +0900

    Translate comments to English

--- main.go
+++ main.go
@@ -35,9 +35,9 @@ func main() {
 		fmt.Println("The number of arguments specified is incorrect.")
 		os.Exit(1)
 	} else {
-		dir = os.Args[1] // 起点となるディレクトリ
+		dir = os.Args[1] // Root directory
 		var err error
-		fp, err = os.Open(os.Args[2]) // ファイル名置換リスト
+		fp, err = os.Open(os.Args[2]) // Filename replacement list
 		if err != nil {
 			panic(err)
 		}
@@ -51,14 +51,14 @@ func main() {
 	for scanner.Scan() {
 		base := scanner.Text()
 		sp := strings.Split(base, "\t")
-		src := sp[0] // 置換リストの1列目（この列の項目をディレクトリ内で探す）
-		dst := sp[1] // 置換リストの2列目（この列の項目に置換する）
+		src := sp[0] // First column of the replacement list (look for items in this column in the directory)
+		dst := sp[1] // Second column of the replacement list (replace with the item in this column)
 
 		for _, path := range paths {
 			dirname := filepath.Dir(path)
 			filebasename := filepath.Base(path)
 
-			// ファイル名置換
+			// File name replacement
 			if filebasename == src {
 				if err := os.Rename(path, dirname+"\\"+dst); err != nil {
 					fmt.Println(err)
